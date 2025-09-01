55b7e911945c8821526e57f0c4c70bbb4aec9a6a
riteshkumar <riteshkumar.g.s@intimetec>
2025-09-01 13:43:28 +0530

    commit for 18-08-2025

---
+++ practice/week1/18-08-2025/SwitchExample.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() 
+{
+    int day;
+    printf("Enter a number (1-7) for the day of the week: ");
+    scanf("%d", &day);
+    switch (day) 
+    {
+        case 1:
+            printf("It's Sunday.\n");
+            break;
+        case 2:
+            printf("It's Monday.\n");
+            break;
+        case 3:
+            printf("It's Tuesday.\n");
+            break;
+        case 4:
+            printf("It's Wednesday.\n");
+            break;
+        case 5:
+            printf("It's Thursday.\n");
+            break;
+        case 6:
+            printf("It's Friday.\n");
+            break;
+        case 7:
+            printf("It's Saturday.\n");
+            break;
+        default:
+            printf("Invalid day entered.\n");
+            break;
+    }
+    return 0;
+}
